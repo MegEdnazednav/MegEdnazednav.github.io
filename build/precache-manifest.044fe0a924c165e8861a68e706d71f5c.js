0cd4e1f7a5f89c744e362841017e4f865c4b8481
Meg Vandezande <meg.vandezande@gmail.com>
2021-03-02 13:10:33 +0100

    new build

---
+++ build/precache-manifest.044fe0a924c165e8861a68e706d71f5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a01d4f2ffbea1bd3055cdc0ee05f3f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3f73c10a5339d787d060",
+    "url": "/static/css/main.5ce41067.chunk.css"
+  },
+  {
+    "revision": "87fdd4b9f7911497c8b1",
+    "url": "/static/js/2.fbf0dea7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fbf0dea7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f73c10a5339d787d060",
+    "url": "/static/js/main.3c70cacc.chunk.js"
+  },
+  {
+    "revision": "e8645f5ba0759e64c84d",
+    "url": "/static/js/runtime-main.f2f4b072.js"
+  }
+]);
